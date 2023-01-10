8bf50c37c3cf047fa897c1eeaaf18cb406d998bd
chenpan <chenpan@smcaiot.com>
2023-01-10 17:15:25 +0800

    feat:掘金 参考文档

--- src/main/java/com/cpiwx/nettydemo/NettyDemoApplication.java
+++ src/main/java/com/cpiwx/nettydemo/NettyDemoApplication.java
@@ -3,6 +3,9 @@ package com.cpiwx.nettydemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 掘金 参考文档 netty-im https://juejin.cn/post/7155097684901101599
+ */
 @SpringBootApplication
 public class NettyDemoApplication {
 
