8c15e2108894ab4b61fa085e075bc4bc6f7daab1
chenpan <chenpan@smcaiot.com>
2023-01-12 09:26:01 +0800

    feat:@EnableRetry

--- src/main/java/com/cpiwx/nettydemo/NettyDemoApplication.java
+++ src/main/java/com/cpiwx/nettydemo/NettyDemoApplication.java
@@ -3,12 +3,14 @@ package com.cpiwx.nettydemo;
 import com.cpiwx.nettydemo.anaotations.EnableNetty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 /**
  * 掘金 参考文档 netty-im https://juejin.cn/post/7155097684901101599
  */
 @SpringBootApplication
 @EnableNetty
+// @EnableRetry
 public class NettyDemoApplication {
 
     public static void main(String[] args) {
